bea82075d5aecea5bf62d317a805487ac154415a
Rambo Swampy <vramaswamy@gmail.com>
2012-04-24 21:44:44 -0700

    cleaned up and commented xhtml parser code since this is vital and somewhat complex

--- src/main/java/com/temula/location/SpaceParserVTDXML.java
+++ src/main/java/com/temula/location/SpaceParserVTDXML.java
@@ -14,14 +14,31 @@ import java.util.regex.Pattern;
 import com.ximpleware.AutoPilot;
 import com.ximpleware.VTDGen;
 import com.ximpleware.VTDNav;
-
+/**
+ * This classes uses VTDXML as the xpath engine to parse xhtml Space representations.  In time trials I ran, this performed 20x faster than Tidy or SAXON.  Supposed to be much faster than Xerxes as well but I haven't timed this.
+ * @author vivekr
+ */
 public class SpaceParserVTDXML implements SpaceParser {
+
+	/** Local logger **/
 	static final Logger logger = Logger.getLogger(SpaceParserVTDXML.class.getName());
+
+	/** Fields will be a static variable shared by all classes.  This might be a bottlneck but I'm not sure yet */
 	static final Field fields[] = Space.class.getDeclaredFields();
+
+	/** Methods loaded for fast, shared lookup.  Again, this might be a performance bottleneck as load increases */
 	static final Method methods[] = Space.class.getMethods();
+
+	/** Maps the itemprop name in the xhtml document to the field associated with it*/
 	static Map<String,Field>nameField=new HashMap<String,Field>();
+
+	/** Maps the itemprop name in the xhtml document to the method associated with it*/
 	static Map<String,Method>nameMethod=new HashMap<String,Method>();
+
+	/** I need a dictionary of what accounts for acceptable boolean term */
 	static Map<String,Boolean>acceptableBooleans = new HashMap<String,Boolean>();
+
+	/** Mapping names to Fields and Methods here.  Note that lowercase itemprop names exactly match to the field names in the Space class */
 	static{
 		for(Field field:fields){
 			String fNameLC = field.getName().toLowerCase();
@@ -35,7 +52,8 @@ public class SpaceParserVTDXML implements SpaceParser {
 			}
 		}
 	}
-	
+
+	/** Setting acceptable boolean values here **/
 	static{
 		acceptableBooleans.put("t",true);
 		acceptableBooleans.put("f",false);
@@ -47,9 +65,21 @@ public class SpaceParserVTDXML implements SpaceParser {
 		acceptableBooleans.put("no",false);
 	}
 	 
-	 
-	Pattern pattern =  Pattern.compile("\\<span itemprop=\"(\\w+)\">(.+)</span>");	 
-	Pattern pattern2 =  Pattern.compile("\\<span itemprop=\"spaceId\"><a href='(.+)'>.+</a></span>");	 
+	/** This is the pattern used to parse fragments.  Essentially looking for the item prop and its value **/
+	Pattern itemPropPattern =  Pattern.compile("\\<span itemprop=\"(\\w+)\">(.+)</span>");	 
+
+	
+	/**
+	 * This method performs the following functionality
+	 * 1) Breaks apart the xhtml into elements by EXPRESSION. In XPATH notation, this would be a node-set
+	 * 2) For each element that results
+	 * 2a) Create a space object
+	 * 2b) Split the element into fragments using SUBEXPRESSION (another nodeset)
+	 * 2c) For each fragment
+	 * 2ci) Test if matches the itemPropPattern.  If so, take the name and value and bind it to the instantiated Space object.
+	 * 2d) Add the space object to the list
+	 * 3) return the list.
+	 */
 	@Override
 	public List<Space> parseXHTML(String xhtml) throws Exception {
 		List<Space> spaces = new ArrayList<Space>();
@@ -86,21 +116,23 @@ public class SpaceParserVTDXML implements SpaceParser {
 				baos1.write(bytes, offset1, len1); //write the fragment out into out.txt
 				byte[]bytes1 = baos1.toByteArray();
 				String fragment = new String(bytes1);
-				Matcher m = pattern.matcher(fragment);
+				Matcher m = itemPropPattern.matcher(fragment);
 				if(m.matches()){
-					if(m.group(1).toLowerCase().equals("spaceid"))continue;
 					this.setValue(m.group(1), m.group(2), space);
-				}
-				Matcher m2 = pattern2.matcher(fragment);
-				if(m2.matches()){
-					//System.out.println(m2.group(1));
 				}
 			}
 			spaces.add(space);
 		}
 		return spaces;
 	}
-
+	
+	/**
+	 * This method sets the value on the space object.  uses introspection so I can change space attributes at will and it will automagically bind.
+	 * @param fieldName
+	 * @param value
+	 * @param space
+	 * @throws Exception
+	 */
 	private void setValue(String fieldName,String value,Space space)throws Exception{
 		String fieldNameLC = fieldName.toLowerCase();
 		Method method = nameMethod.get(fieldNameLC);
